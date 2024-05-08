b5889531cfcf5c2c91e7bae7ce74f33585aa7f20
bosemrinali <158981114+bosemrinali@users.noreply.github.com>
2024-05-08 14:23:04 +0530

    Add files via upload

---
+++ DSG0124.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print("Hello World!")
+
+
+# In[4]:
+
+
+print("Mrinali Bose")
+
+
+# In[5]:
+
+
+10+20
+
+
+# In[6]:
+
+
+20-10
+
+
+# In[7]:
+
+
+10*20
+
+
+# In[8]:
+
+
+20/10
+
+
+# SQL,Machine Learning,Python
+
+# In[ ]:
+
+
+
+
